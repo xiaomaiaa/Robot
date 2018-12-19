82e02603f57b8e9bbab7bcb614839bea0440a992
xiaomaiaa <44515853+xiaomaiaa@users.noreply.github.com>
2018-12-19 06:54:56 -0800

    Add files via upload

---
+++ V4.0/scripts/junk_move1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from junk_move_util import *
+junk_id=1
+a=jm(junk_id)
+a.do()
---
+++ V4.0/scripts/junk_move3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from junk_move_util import *
+junk_id=3
+a=jm(junk_id)
+a.do()
